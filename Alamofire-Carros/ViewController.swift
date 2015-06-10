b9b85a504cd27b737dd9f3489ef3383c8b26c41a
Quaddro Admin <quaddroadmin@WozMac9-2.local>
2015-06-10 20:07:03 -0300

    Initial Commit

---
+++ Alamofire-Carros/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Alamofire-Carros
+//
+//  Created by Quaddro Admin on 10/06/15.
+//  Copyright (c) 2015 Quaddro Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
